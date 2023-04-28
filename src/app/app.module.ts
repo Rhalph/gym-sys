deb9bfdefc27e3b535f046ba29c4eccc625b92c0
moonface01 <benson.gais@gmail.com>
2023-04-28 04:19:59 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { RegistrListComponent } from './registr-list/registr-list.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
@@ -28,6 +30,8 @@ import { NgConfirmModule } from 'ng-confirm-box';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
 
     NgToastModule,
     NgConfirmModule
